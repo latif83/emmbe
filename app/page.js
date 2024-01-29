1a6121f753d26e87c16cca1c46607d1418dc52f4
latif83 <latifm8360@gmail.com>
2024-01-29 16:52:48 +0000

    update

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import {
   faFacebook,
   faInstagram,
@@ -44,27 +44,40 @@ export default function Home() {
     <div className="w-full overflow-hidden">
       <Header />
       <Banner />
-      <section 
+      <section
         className={`grid md:grid-cols-3 grid-cols-1 sm:gap-4 gap-8 sm:px-12 px-5 py-7 bg-white ${styles.mission}`}
       >
         <div className="col-span-1 relative z-10 flex justify-center flex-col gap-4">
-          <motion.p initial={{ opacity: 0, x: -100 }}
-                whileInView={{ opacity: 1, x: 0 }}
-                transition={{ duration: 1 }} className="text-yellow-500 font-bold mb-4">OUR MISSION</motion.p>
-          <motion.h1 initial={{ opacity: 0, x: -100 }}
-                whileInView={{ opacity: 1, x: 0 }}
-                transition={{ duration: 1 }} className="text-3xl font-bold">QUALITY WORK IS OUR PRIORITY</motion.h1>
-          <motion.p initial={{ opacity: 0, y: -100 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                transition={{ duration: 1 }}>
+          <motion.p
+            initial={{ opacity: 0, x: -100 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1 }}
+            className="text-yellow-500 font-bold mb-4"
+          >
+            OUR MISSION
+          </motion.p>
+          <motion.h1
+            initial={{ opacity: 0, x: -100 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1 }}
+            className="text-3xl font-bold"
+          >
+            QUALITY WORK IS OUR PRIORITY
+          </motion.h1>
+          <motion.p
+            initial={{ opacity: 0, y: -100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+          >
             EMMBE Design and Construction Enterprise strives to advance Ghana's
             economy and society through sustainable infrastructure. We
             prioritize quality, cost-effectiveness, and first-className services
             for our customers.
           </motion.p>
-          <motion.button initial={{ opacity: 0, y: -100 , scale:0.5 }}
-                whileInView={{ opacity: 1, y: 0,scale:1 }}
-                transition={{ duration: 1 }}
+          <motion.button
+            initial={{ opacity: 0, y: -100, scale: 0.5 }}
+            whileInView={{ opacity: 1, y: 0, scale: 1 }}
+            transition={{ duration: 1 }}
             type="button"
             className="text-gray-900 bg-gradient-to-r from-yellow-200 via-yellow-400 to-yellow-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 shadow-lg shadow-yellow-500/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
           >
@@ -72,9 +85,10 @@ export default function Home() {
           </motion.button>
         </div>
         <div className="col-span-2 relative z-10 grid grid-cols-2 gap-2 relative">
-          <motion.div initial={{ opacity: 0, x: -100 }}
-                whileInView={{ opacity: 1, x: 0 }}
-                transition={{ duration: 1 }}
+          <motion.div
+            initial={{ opacity: 0, x: -100 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1 }}
             className={`col-span-1 bg-black ${styles.wImgCon} rounded overflow-hidden`}
           >
             <Image
@@ -84,9 +98,10 @@ export default function Home() {
               className={styles.wImg}
             />
           </motion.div>
-          <motion.div initial={{ opacity: 0, y: 100 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                transition={{ duration: 1 }}
+          <motion.div
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
             className={`col-span-1 bg-black ${styles.wImgCon} rounded overflow-hidden`}
           >
             <Image
@@ -97,9 +112,9 @@ export default function Home() {
             />
           </motion.div>
           <motion.div
-          initial={{ opacity: 0, y: 100 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1 }}
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
             className={`col-span-1 bg-black ${styles.wImgCon} rounded overflow-hidden`}
           >
             <Image
@@ -110,9 +125,10 @@ export default function Home() {
               style={{ objectPosition: "center" }}
             />
           </motion.div>
-          <motion.div initial={{ opacity: 0, x: 100 }}
-                whileInView={{ opacity: 1, x: 0 }}
-                transition={{ duration: 1 }}
+          <motion.div
+            initial={{ opacity: 0, x: 100 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1 }}
             className={`col-span-1 bg-black ${styles.wImgCon} rounded overflow-hidden`}
           >
             <Image
@@ -122,9 +138,12 @@ export default function Home() {
               className={styles.wImg}
             />
           </motion.div>
-          <motion.div initial={{ opacity: 0, y: -100 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                transition={{ duration: 1 }} className={`${styles.wAbs} flex items-center justify-center`}>
+          <motion.div
+            initial={{ opacity: 0, y: -100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+            className={`${styles.wAbs} flex items-center justify-center`}
+          >
             <div className="rounded-full bg-white sm:px-4 sm:py-12 px-4 py-3 text-center">
               <p className="sm:text-5xl text-3xl font-bold text-yellow-400">
                 8+
@@ -143,40 +162,54 @@ export default function Home() {
       </section>
 
       <div className="sm:px-12 px-5 grid grid-cols-2 gap-4 py-5">
-        <div className="col-span-1">
-
+        <div className="col-span-1 flex items-center">
+            <p className="text-gray-600 text-lg font-bold">
+              Explore our construction projects and witness the art of building
+              dreams into reality.
+            </p>
         </div>
-      <div className="col-span-1">
-      <video className="w-full h-[300px] object-cover rounded-lg" controls>
-  <source src="/vid1.mp4" type="video/mp4" />
-  Your browser does not support the video tag.
-</video>
-
-      </div>
+        <div className="col-span-1">
+          <video className="w-full h-[300px] object-cover rounded-lg" controls>
+            <source src="/vid1.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
       </div>
 
       <section className="sm:px-12 px-5">
         <div className={`${styles.ww} flex items-center gap-2 p-5 py-10`}>
-          <motion.div initial={{ opacity: 0, x: -100 }}
-                whileInView={{ opacity: 1, x: 0 }}
-                transition={{ duration: 1 }} className={`relative z-10 ${styles.wwch1}`}>
+          <motion.div
+            initial={{ opacity: 0, x: -100 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1 }}
+            className={`relative z-10 ${styles.wwch1}`}
+          >
             <h1 className="text-2xl font-bold">
               Building your visions. Creating reality.
             </h1>
             <p className="text-gray-600">
-            With expertise in architectural and structural design, project estimation, construction, renovations, and more, we're committed to transforming your dreams into tangible reality. Explore our services and let's build together!
+              With expertise in architectural and structural design, project
+              estimation, construction, renovations, and more, we're committed
+              to transforming your dreams into tangible reality. Explore our
+              services and let's build together!
             </p>
             <button
               type="button"
               className="text-gray-900 bg-gradient-to-r mt-5 from-blue-200 via-blue-400 to-blue-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 shadow-lg shadow-blue-500/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center flex gap-2 items-center justify-center"
             >
               DISCOVER MORE
-              <FontAwesomeIcon icon={faRightLong} width={20} height={20} className="text-lg" />
+              <FontAwesomeIcon
+                icon={faRightLong}
+                width={20}
+                height={20}
+                className="text-lg"
+              />
             </button>
           </motion.div>
-          <motion.div initial={{ opacity: 0, scale: 0.5 }}
-                whileInView={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 1 }}
+          <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 1 }}
             className={`relative z-10 ${styles.wwch2}`}
             style={{ height: "300px" }}
           >
@@ -201,14 +234,20 @@ export default function Home() {
       </section>
 
       <section className="bg-yellow-400 sm:px-12 px-5 py-12">
-        <motion.h1 initial={{ opacity: 0, y: -100 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                transition={{ duration: 1 }} className="text-white font-bold text-2xl mt-3">
+        <motion.h1
+          initial={{ opacity: 0, y: -100 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+          className="text-white font-bold text-2xl mt-3"
+        >
           We offer comprehensive services
         </motion.h1>
-        <motion.div initial={{ opacity: 0, scale: 0.5 }}
-                whileInView={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 1 }} className="grid sm:grid-cols-2 md:grid-cols-3 grid-cols-1 gap-4 sm:mt-10 mt-6">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 1 }}
+          className="grid sm:grid-cols-2 md:grid-cols-3 grid-cols-1 gap-4 sm:mt-10 mt-6"
+        >
           <div className="col-span-1 shadow-lg p-5 rounded-lg bg-white flex flex-col justify-center items-center gap-4">
             <div>
               <span className="bg-black text-white inline-block rounded-full px-3 py-4">
@@ -305,9 +344,12 @@ export default function Home() {
             </div>
           </div>
         </motion.div>
-        <motion.div initial={{ opacity: 0, y: -100 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                transition={{ duration: 1 }} className="mt-3 flex items-center justify-center">
+        <motion.div
+          initial={{ opacity: 0, y: -100 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+          className="mt-3 flex items-center justify-center"
+        >
           <button
             type="button"
             className="text-gray-900 bg-gradient-to-r mt-5 from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 shadow-lg shadow-lime-500/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center flex gap-2"
@@ -322,9 +364,12 @@ export default function Home() {
           </button>
         </motion.div>
       </section>
-      <motion.section initial={{ opacity: 0, y: -100 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                transition={{ duration: 1 }} className="bg-white relative sm:px-12 px-5">
+      <motion.section
+        initial={{ opacity: 0, y: -100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+        className="bg-white relative sm:px-12 px-5"
+      >
         <div className="bg-yellow-400 p-5 absolute top-0 left-0 w-full z-0"></div>
 
         <div className="relative z-10 bg-white sm:p-12 border-4 border-yellow-400 p-5 rounded">
@@ -392,18 +437,27 @@ export default function Home() {
         <div className="bg-yellow-400 p-5 absolute bottom-0 left-0 w-full z-0"></div>
       </motion.section>
       <section className="text-neutral-700 sm:px-12 px-5 py-10 bg-yellow-400">
-        <motion.div initial={{ opacity: 0, x: -100 }}
-                whileInView={{ opacity: 1, x: 0 }}
-                transition={{ duration: 1 }} className="mx-auto text-center md:max-w-xl lg:max-w-3xl">
+        <motion.div
+          initial={{ opacity: 0, x: -100 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1 }}
+          className="mx-auto text-center md:max-w-xl lg:max-w-3xl"
+        >
           <h3 className="mb-6 text-3xl font-bold">Testimonials</h3>
           <p className="mb-6 pb-2 md:mb-12 md:pb-0">
-          Listen to the love from our valued customers who have experienced the excellence of EMMBE Design and Construction Enterprise. Read their testimonials and discover the satisfaction that comes from building dreams together.
+            Listen to the love from our valued customers who have experienced
+            the excellence of EMMBE Design and Construction Enterprise. Read
+            their testimonials and discover the satisfaction that comes from
+            building dreams together.
           </p>
         </motion.div>
 
-        <motion.div initial={{ opacity: 0, x: 100 }}
-                whileInView={{ opacity: 1, x: 0 }}
-                transition={{ duration: 1 }} className="grid gap-6 text-center md:grid-cols-3">
+        <motion.div
+          initial={{ opacity: 0, x: 100 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1 }}
+          className="grid gap-6 text-center md:grid-cols-3"
+        >
           <div>
             <div className="block rounded-lg bg-white shadow-lg">
               <div className="h-28 overflow-hidden rounded-t-lg bg-[#9d789b]"></div>
@@ -423,7 +477,11 @@ export default function Home() {
                   >
                     <path d="M13 14.725c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275zm-13 0c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275z" />
                   </svg>
-                  Exceptional work by EMMBE Design and Construction! The team went above and beyond in turning our vision into reality. From detailed architectural plans to flawless construction, their professionalism and attention to detail were outstanding. Highly recommend for anyone looking to build their dream home.
+                  Exceptional work by EMMBE Design and Construction! The team
+                  went above and beyond in turning our vision into reality. From
+                  detailed architectural plans to flawless construction, their
+                  professionalism and attention to detail were outstanding.
+                  Highly recommend for anyone looking to build their dream home.
                 </p>
               </div>
             </div>
@@ -436,7 +494,9 @@ export default function Home() {
                 <img src="/avatar.png" />
               </div>
               <div className="p-6">
-                <h4 className="mb-4 text-2xl font-semibold">Mrs.Abena Lawson</h4>
+                <h4 className="mb-4 text-2xl font-semibold">
+                  Mrs.Abena Lawson
+                </h4>
                 <p>Tema - Ghana</p>
                 <hr />
                 <p className="mt-4">
@@ -448,7 +508,11 @@ export default function Home() {
                   >
                     <path d="M13 14.725c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275zm-13 0c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275z" />
                   </svg>
-                  Working with EMMBE was a pleasure. Their expertise in project estimation and efficient construction management made our commercial project a success. The team's dedication to quality and meeting deadlines is commendable. I look forward to future collaborations.
+                  Working with EMMBE was a pleasure. Their expertise in project
+                  estimation and efficient construction management made our
+                  commercial project a success. The team's dedication to quality
+                  and meeting deadlines is commendable. I look forward to future
+                  collaborations.
                 </p>
               </div>
             </div>
@@ -473,14 +537,18 @@ export default function Home() {
                   >
                     <path d="M13 14.725c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275zm-13 0c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275z" />
                   </svg>
-                  EMMBE transformed our outdated space into a modern masterpiece. The renovation process was smooth, and their commitment to delivering top-notch craftsmanship was evident throughout. The communication and transparency exhibited by the team made the entire experience stress-free. Great job!
+                  EMMBE transformed our outdated space into a modern
+                  masterpiece. The renovation process was smooth, and their
+                  commitment to delivering top-notch craftsmanship was evident
+                  throughout. The communication and transparency exhibited by
+                  the team made the entire experience stress-free. Great job!
                 </p>
               </div>
             </div>
           </div>
         </motion.div>
       </section>
-      
+
       <Contact />
       <Footer />
     </div>
